840cfad14f31b74e908e89fbcae493908a4280d1
kim-se-hee <saree98@naver.com>
2026-02-21 19:31:13 +0900

    init: 최초 커밋

---
+++ src/main/java/ksh/tryptobackend/TryptoBackendApplication.java
@@ -0,0 +1,13 @@
+package ksh.tryptobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryptoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryptoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ksh/tryptobackend/TryptoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ksh.tryptobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryptoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
