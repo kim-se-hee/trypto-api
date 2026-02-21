df17830a6e588209f784e1ce2f8a54256df6b6ab
kim-se-hee <saree98@naver.com>
2026-02-22 04:54:13 +0900

    feat: CEX 주문 에러 코드 추가

--- src/main/java/ksh/tryptobackend/common/exception/ErrorCode.java
+++ src/main/java/ksh/tryptobackend/common/exception/ErrorCode.java
@@ -8,6 +8,17 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
 
     INTERNAL_SERVER_ERROR(500, "internal.server.error"),
+
+    INSUFFICIENT_BALANCE(400, "insufficient.balance"),
+    BELOW_MIN_ORDER_AMOUNT(400, "below.min.order.amount"),
+    ABOVE_MAX_ORDER_AMOUNT(400, "above.max.order.amount"),
+    PRICE_REQUIRED_FOR_LIMIT(400, "price.required.for.limit"),
+    ORDER_NOT_CANCELLABLE(400, "order.not.cancellable"),
+
+    WALLET_NOT_FOUND(404, "wallet.not.found"),
+    EXCHANGE_COIN_NOT_FOUND(404, "exchange.coin.not.found"),
+    EXCHANGE_NOT_FOUND(404, "exchange.not.found"),
+    ORDER_NOT_FOUND(404, "order.not.found"),
     ;
 
     private final int status;
