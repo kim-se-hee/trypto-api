5e8b09161ac6ec5360686a7e98716c4ef7e36c3d
kim-se-hee <saree98@naver.com>
2026-03-13 05:00:21 +0900

    feat: User Output Port 추가 - UserQueryPort (조회), UserCommandPort (쓰기) 분리

---
+++ src/main/java/ksh/tryptobackend/user/application/port/out/UserCommandPort.java
@@ -0,0 +1,10 @@
+package ksh.tryptobackend.user.application.port.out;
+
+import ksh.tryptobackend.user.domain.model.User;
+
+public interface UserCommandPort {
+
+    User save(User user);
+
+    boolean existsByNickname(String nickname);
+}
---
+++ src/main/java/ksh/tryptobackend/user/application/port/out/UserQueryPort.java
@@ -0,0 +1,10 @@
+package ksh.tryptobackend.user.application.port.out;
+
+import ksh.tryptobackend.user.domain.model.User;
+
+import java.util.Optional;
+
+public interface UserQueryPort {
+
+    Optional<User> findById(Long userId);
+}
