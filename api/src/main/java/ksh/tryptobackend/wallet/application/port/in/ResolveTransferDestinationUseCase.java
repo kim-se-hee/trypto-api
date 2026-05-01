262ecb3833dcb197a5ccd18b1b23c9ee9a349c4d
kim-se-hee <saree98@naver.com>
2026-05-01 19:23:45 +0900

    refactor: wallet 미사용 ResolveTransferDestination UseCase/Service 제거

--- api/src/main/java/ksh/tryptobackend/wallet/application/port/in/ResolveTransferDestinationUseCase.java
+++
@@ -1,8 +0,0 @@
-package ksh.tryptobackend.wallet.application.port.in;
-
-import ksh.tryptobackend.wallet.application.port.in.dto.result.TransferDestinationResult;
-
-public interface ResolveTransferDestinationUseCase {
-
-    TransferDestinationResult resolveDestination(Long roundId, String toAddress);
-}
